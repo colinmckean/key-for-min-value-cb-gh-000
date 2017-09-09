3ef648b56b2440a69b5fdc3eea0832fc1c1aa66c
Colin McKean <ezikill@hotmail.com>
2017-09-09 12:03:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-return_key = ""
+return_key = nil
 return_value = 1000
 
 name_hash.each {|key, value|
