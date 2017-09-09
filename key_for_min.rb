fc66d74b7a074db88d62375db95c60b18dc09b6f
Colin McKean <ezikill@hotmail.com>
2017-09-09 11:55:43 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
 return_key = ""
 return_value = 0
 
-name_hash.each {|key, value| 
-  if value > return_value 
+name_hash.each {|key, value|
+  if value > return_value
     return_key = key
   end
  }
